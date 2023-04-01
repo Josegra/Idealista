674b21f4ffb29c2bebd3ddb691042f8fe34784ff
Jose Gonzalez <josegra1163@gmail.com>
2023-04-02 00:50:50 +0200

    Add files via upload

---
+++ idealista/spiders/idealista_scrape.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class IdealistaScrapeSpider(scrapy.Spider):
+    name = 'idealista_scrape'
+    allowed_domains = ['idealista.com']
+    start_urls = ['https://www.idealista.com/geo/alquiler-viviendas/barrio-de-la-latina/']
+    page_number = 2
+
+    def parse(self, response):
+        for houses in response.css('.item-info-container'):
+            yield {
+                'title' : houses.css('a.item-link::text').get(),
+                'price' : houses.css('span.item-price.h2-simulated::text').get(),
+                'rooms' : houses.css('span.item-detail:nth-child(1)::text').get(),
+                'mtts' : houses.css('span.item-detail:nth-child(2)::text').get(),
+                'floor' : houses.css('span.item-detail:nth-child(3)::text').get() ,
+                'link' : houses.css('a.item-link::attr(href)').get(),
+                'description' : houses.css('.ellipsis::text').get(),
+            }
+
+        next_page = 'https://www.idealista.com/alquiler-viviendas/madrid/moncloa/pagina-' + str(IdealistaScrapeSpider.page_number) + '.htm'
+        if IdealistaScrapeSpider.page_number < 20:
+            IdealistaScrapeSpider.page_number += 1
+            yield response.follow(next_page, callback=self.parse)
